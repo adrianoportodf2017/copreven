f6f9cf95039bb616951873ca74796c76099e54bc
adriano alves <33916451+adrianoportodf2017@users.noreply.github.com>
2024-04-18 11:07:22 -0300

    adicionando variaveis para contrato

--- pages/salas.php
+++ pages/salas.php
@@ -17,7 +17,7 @@ $consulta = select("atendimento", "*", "WHERE dia LIKE '%$dia%' AND mes LIKE '%$
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="refresh" content="25;URL=salas.php">
+  <meta http-equiv="refresh" content="7;URL=salas.php">
 
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
