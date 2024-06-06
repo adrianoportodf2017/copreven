21e0ce7cfc1842ef13e7c0b1f629100fb77e087e
adriano <adrianobr00@gmail.com>
2024-06-06 12:26:10 -0300

    atualizando guia 305

--- pages/guia_305.php
+++ pages/guia_305.php
@@ -1,5 +1,3 @@
-
-
 <?php
 
 //DADOS via URL ...
@@ -33,57 +31,52 @@ $eeg              = @$_GET["eeg"];
 
 <!DOCTYPE html>
 <html lang="pt-BR">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    
-    <title>GUIA DE ATENDIMENTO (SALA 305) | PREVEN</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-  </head>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>GUIA DE ATENDIMENTO (SALA 305) | PREVEN</title>
+	<style>
+		.exam-item {
+			margin: 2px;
+			border-bottom: 1px solid #000000 !important;
+			font-size: 12px;
+		}
+	</style>
+</head>
 
 <body onload="self.print();">
-
-<div class="container">
-	<div class="row">
-	
-		<div class="col-md-12">
-			
-			<blockquote>
-			<h6>
-			Colaborador: <strong><?php echo "$nome";?></strong><br>	
-			RG / CPF: <strong><?php echo "$rg";?></strong><br>
-			Convênio: <strong><?php echo "$convenio";?></strong><br>
-			Empresa: <strong><?php echo "$empresa";?></strong><br>
-			Data de Nascimento: <strong><?php echo "$nascimento";?></strong><br>		
-			Tipo de exame: <strong><?php echo "$tipo";?></strong><br>
-			Função: <strong><?php echo "$cargo";?></strong>
-			</h6>
-			<span class="glyphicon glyphicon-triangle-right" aria-hidden="true">Endereço: Sala 305 - 3º Andar.</span>
-			</blockquote>
-
+	<div class="">
+		<div class="row">
+			<div class="">
+				<h4>
+					Colaborador: <strong><?php echo "$nome"; ?></strong><br>
+					RG / CPF: <strong><?php echo "$rg"; ?></strong><br>
+					Convênio: <strong><?php echo "$convenio"; ?></strong><br>
+					Empresa: <strong><?php echo "$empresa"; ?></strong><br>
+					Data de Nascimento: <strong><?php echo "$nascimento"; ?></strong><br>
+					Tipo de exame: <strong><?php echo "$tipo"; ?></strong><br>
+					Função: <strong><?php echo "$cargo"; ?></strong>
+					<span class="glyphicon glyphicon-triangle-right" aria-hidden="true">Endereço: Sala 305 - 3º Andar.</span>
+				</h4>
+			</div>
+			<div class="">
+				<div class="row">
+					<?php
+					$espaço = '<p><br>';
+					if (!empty($tem_clinico)) {
+						echo '<div class="col-12 exam-item">[ <strong>' . $tem_torax . '</strong> ] EXAME CLÍNICO ' . $espaço . '</div>';
+					}
+					if (!empty($tem_romberg)) {
+						echo '<div class="col-12 exam-item">[ <strong>' . $tem_romberg . '</strong> ] ROMBERG ' . $espaço . '</div>';
+					}
+					?>
+				</div>
+			</div>
 		</div>
-
-		<div class="col-md-12">
-			
-			<blockquote>
-			<h5>
-			
-			[ <strong><?php echo $tem_clinico ?></strong> ] EXAME CLÍNICO<br>
-            [ <strong><?php echo $tem_romberg ?></strong> ] ROMBERG
-			
-			</h5>
-			</blockquote>
-
-		</div>
-	</div>	
-</div>
-
-
+	</div>
 </body>
-
-<?php include("../footer.php"); ?>
-  </html>
-  
+</html>
