6421dc6140159b5d4d5175929202755b2a894203
adriano <adrianobr00@gmail.com>
2024-07-19 11:21:43 -0300

    atuaçizando login

--- functions/validate_login.php
+++ functions/validate_login.php
@@ -1,21 +1,28 @@
 <?php
 session_start();
+include("../includes/autoload.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
     // Use http instead of https
-    $api_url = 'adm/api/login';
+    $api_url = $base_url.'adm/public/api/login';
     $data = array('email' => $email, 'password' => $password);
     $options = array(
         'http' => array(
             'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
             'method'  => 'POST',
             'content' => http_build_query($data),
+            'verify_peer' => false,
+            'verify_peer_name' => false,
         ),
+        "ssl" => array(
+            "verify_peer" => false,
+            "verify_peer_name" => false,
+        )
     );
     $context  = stream_context_create($options);
-    $result = file_get_contents($api_url, false, $context);
+    @$result = file_get_contents($api_url, false, $context);
 
     if ($result === FALSE) {
         echo json_encode(array('success' => false));
@@ -29,3 +36,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo json_encode(array('success' => false, 'return' => $response ));
     }
 }
+?>
